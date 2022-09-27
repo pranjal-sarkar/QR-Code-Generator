ae0f1d454a06934933809e48ca7070f6ed393b2d
Pranjal <tataisarkar2001@gmail.com>
2022-09-27 14:36:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const qrcode = require("qrcode");
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+
+app.get('/', (req, res, next)=>{
+    res.render("index");
+})
+
+app.post('/scan', (req, res, next)=>{
+    const input_text = req.body.text;
+    console.log(input_text);
+    qrcode.toDataURL(input_text, (err, src) => {
+        res.render("scan", {
+            qr_code: src,
+        });
+    })
+})
+
+app.listen(port, console.log(`Listening on port ${port}`));
